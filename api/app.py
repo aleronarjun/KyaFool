b57d1900f1a24c439293507126a0483a2c2c352a
Rasik <rasikraj01@gmail.com>
2018-01-21 23:28:39 +0530

    image file uploads

--- api/app.py
+++ api/app.py
@@ -1,13 +1,35 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
-    return render_template('index.html')
+    return render_template('upload.html')
+
+
+
+APP_PATH = os.path.dirname(os.path.abspath(__file__));
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    target = os.path.join(APP_PATH, 'images/')
+    print(target)
+    print(request)
+
+    if not os.path.isdir(target):
+        os.mkdir(target)
+
+    for file in request.files.getlist("file"):
+        print(file)
+        filename = file.filename
+        destination = "/".join([target, filename])
+        print(destination)
+        file.save(destination)
+
+    return render_template('success.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
